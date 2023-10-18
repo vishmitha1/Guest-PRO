25c73f4a9ab17f02fdc763ea616dcca750413006
visal <visaluni2@gmail.com>
2023-10-18 23:23:01 +0530

    includes

---
+++ app/views/includes/components/sidenavbar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+    <link rel="stylesheet" href='<?php echo URLROOT; ?>/public/css/side-navbar/navbar.css' >
+</head>
+<body>
+    <div class="side-bar">
+        <a href="#">Reservations</a>
+        <a href="#">Reservations</a>
+        <a href="#">Reservations</a>
+        <a href="#">Reservations</a>
+        <a href="#">Reservations</a>
+    </div>
+    
