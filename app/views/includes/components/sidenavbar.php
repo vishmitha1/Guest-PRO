c0b468cec4491e08eec1b108cc31b70f9c2b5db2
visal <visaluni2@gmail.com>
2023-10-19 15:13:16 +0530

    Sidenav bar created

--- app/views/includes/components/sidenavbar.php
+++ app/views/includes/components/sidenavbar.php
@@ -6,13 +6,32 @@
     <title>Document</title>
    
     <link rel="stylesheet" href='<?php echo URLROOT; ?>/public/css/side-navbar/navbar.css' >
+    <script src="https://kit.fontawesome.com/e2b0a95ef4.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="side-bar">
-        <a href="#">Reservations</a>
-        <a href="#">Reservations</a>
-        <a href="#">Reservations</a>
-        <a href="#">Reservations</a>
-        <a href="#">Reservations</a>
+        <div class="logo">
+            <h1><i class="fa-solid fa-hotel fa-beat-fade fa-2xl"></i>  Guest PRO</h1>
+        </div>
+        <div class="links">
+            <a href="#"><i class="fa-solid fa-hotel"></i>Reservations</a>
+            <a href="#"><i class="fa-solid fa-bell-concierge"></i>Food Orders</a>
+            <a href="#"><i class="fa-solid fa-file-invoice"></i>Bill</a>
+            <a href="#"><i class="fa-regular fa-credit-card"></i>Payments</a>
+            <a href="#"><i class="fa-solid fa-cart-flatbed-suitcase"></i>Service Request</a>
+            <a href="#"><i class="fa-solid fa-person-walking-luggage"></i>Complains</a>
+            <a href="#"><i class="fa-solid fa-star"></i></i>Rate</a>
+        </div>
+        <div class="logout">
+             <button  value="logout"><i class="fa-solid fa-right-from-bracket"></i> Logout</button>
+        </div>
+       
+        
+
+
+
     </div>
+
+</body>
+</html>
     
