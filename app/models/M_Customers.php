27a15195184e85f4fca7ea4404906602c2782ed8
visal <visaluni2@gmail.com>
2023-10-28 22:30:09 +0530

    crete modle for get ordrdetails frm dtbse

--- app/models/M_Customers.php
+++ app/models/M_Customers.php
@@ -9,9 +9,10 @@
 
         //place food order
         public function placefoodorder($data){
-            $this->db->query('Insert into foodorders(item_name ,quantity ,note) VALUES(:name ,:quantity ,:note)');
+            $this->db->query('Insert into foodorders(item_name ,quantity ,status,note) VALUES(:name ,:quantity ,:status,:note)');
             $this->db->bind(':name', $data['food']);
             $this->db->bind(':quantity',$data['quantity'] );
+            $this->db->bind(':status',"Placed");
             $this->db->bind(':note',$data['note'] );
             
 
@@ -21,6 +22,29 @@
             else{
                 return false;
             }
+        }
+
+        public function getorderdetails(){
+            $this->db->query("SELECT * FROM foodorders ");
+            
+            $row = $this->db->resultSet();
+            // echo($row[0]->order_id);
+            // echo("<br>".count($row));
+            // echo("<br>");
+            $row=array_reverse($row);
+
+            foreach($row  as $orders){
+            
+                $item=$orders->item_name;
+                $quant=$orders->quantity;
+                $date=$orders->date;
+                $stat=$orders->status;
+                
+            }
+            
+
+            return $row;
+
         }
     }
     
