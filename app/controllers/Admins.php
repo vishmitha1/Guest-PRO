da5a4d63a47f586c6eaa6917890f4339451d8049
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2023-10-29 21:31:16 +0530

    Add admin pages

--- app/controllers/Admins.php
+++ app/controllers/Admins.php
@@ -1,12 +1,26 @@
 <?php
     class Admins extends Controller{
+ 
 
-
-        public function visal(){
+        public function dashboard(){
             $data =[  ];
-            $this->view('users/v_login', $data);
+            $this->view('admin/admin_dashboard', $data);
         }
+
+        public function staffaccounts(){
+            $data =[  ];
+            $this->view('admin/admin_staffaccounts', $data);
+        }
+
+        public function useraccounts(){
+            $data =[  ];
+            $this->view('admin/admin_useraccounts', $data);
+        }
+
+        public function accountlogs(){
+            $data =[  ];
+            $this->view('admin/admin_accountlogs', $data);
+        }
+
     }
-
-
 ?>
---
+++ app/views/includes/components/sidenavbar_admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+    <link rel="stylesheet" href='<?php echo URLROOT; ?>/public/css/mainstyle.css' >
+    <script src="https://kit.fontawesome.com/e2b0a95ef4.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="side-bar">
+        <div class="logo">
+            <h1><i class="fa-solid fa-hotel fa-beat-fade fa-2xl"></i>  Guest PRO</h1>
+        </div>
+        <div class="links">
+            <a href="#"><i class="fa-solid fa-hotel"></i>Dashboard</a>
+            <a href="#"><i class="fa-solid fa-bell-concierge"></i>Staff Accounts</a>
+            <a href="#"><i class="fa-solid fa-file-invoice"></i>User Accounts</a>
+            <a href="#"><i class="fa-regular fa-credit-card"></i>Account Logs</a>
+        </div>
+        <div class="logout">
+             <button  value="logout"><i class="fa-solid fa-right-from-bracket"></i>Logout</button>
+        </div>
+    </div>
+
+
+    
