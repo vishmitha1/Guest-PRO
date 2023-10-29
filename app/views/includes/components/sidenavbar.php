2be62093aa339f0e9828e077b1151d7dec601907
visal <visaluni2@gmail.com>
2023-10-29 22:42:25 +0530

    update

--- app/views/includes/components/sidenavbar.php
+++ app/views/includes/components/sidenavbar.php
@@ -47,12 +47,7 @@
         </div>
     </div>
 
-   <!-- '''''''''''''''''''''''''''''''user account imported'''''''''''''''''''''''''''''''''''''''' -->
-    <div class="home">
-        <div class="profile">
-        <a href="#"><i class="fa-solid fa-user fa-2xl"></i>   Visal Alwis</a>
-        </div>
-    </div>
+   
 
 
     
