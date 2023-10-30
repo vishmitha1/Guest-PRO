d211ef079d6e0eadfd57fa25b0f85bb72faa7911
visal <visaluni2@gmail.com>
2023-10-30 23:44:04 +0530

    add table

--- app/views/customers/v_servicerequest.php
+++ app/views/customers/v_servicerequest.php
@@ -16,5 +16,49 @@
             <textarea name="message" class="message-input" placeholder="Type your message..." rows="4"></textarea>
             <button type="submit" class="send-button">Send</button>
         </form>
+
+        <div class="bill-table-warpper">
+            <div class="main-title"> History</div>
+            <div class="bill-table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            
+                        </tr>
+                    </thead>    
+                        <tbody>
+                            <tr>
+                                <td>2023/04/10</td>
+                                <td>10.25am</td>
+                                <td>front desk with a request for additional towels.</td>
+                                <td>Placed</td>
+                                
+                            </tr>
+                            <tr>
+                                <td>2023/06/18</td>
+                                <td>10.25am</td>
+                                <td>front desk with a request for additional towels.</td>
+                                <td>Placed</td>
+                                
+                            </tr>
+                            <tr>
+                                <td>2023/04/10</td>
+                                <td>10.25pm</td>
+                                <td>front desk with a request for additional towels.</td>
+                                <td>Placed</td>
+                                
+                            </tr>
+                            
+                            
+                           
+                        </tbody>
+                    
+                </table>
+            </div>
+        </div>
     </div>
 
