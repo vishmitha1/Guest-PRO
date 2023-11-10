e7b4017ac01809073733da75363a8291719bd6ac
visal <visaluni2@gmail.com>
2023-11-10 19:23:15 +0530

    create useser sessions with authentications

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -119,29 +119,9 @@
                         //can login
                         $loggeduser=$this->userModel->login($data['email'],$data['password']);
                             
-                        if($loggeduser != false){
+                        if($loggeduser ){
                         
-                            if($loggeduser == "admin"){
-                                redirect("Admins/staffaccounts");
-                            }
-                            elseif($loggeduser == "waiter"){
-                                redirect("Waiters/pendingfoodorders");
-                            }
-                            elseif($loggeduser == "receptionist"){
-                                redirect("Receptionists/reservation");
-                            }
-                            elseif($loggeduser == "supervisor"){
-                                redirect("Supervisors/servicerequest");
-                            }
-                            elseif($loggeduser == "kitchen"){
-                                redirect("Kitchen/foodmenu");
-                            }
-                            elseif($loggeduser == "customer"){
-                                redirect("Customers/reservation");
-                            }
-                            elseif($loggeduser == "manager"){
-                                redirect("Managers/roomdetails");
-                            }
+                            $this->createUsersession($loggeduser);
                         }    
                         else{
                             $data['password_err']= 'Password incorrect';
@@ -172,6 +152,47 @@
                 ];
                 $this->view('users/v_login', $data);
             }
+        }
+
+        public function createUsersession($user){
+            $_SESSION['user_id']=$user ->id;
+            $_SESSION['username']= $user -> name;
+            $_SESSION['email']= $user -> email;
+            $_SESSION['role']= $user -> role;
+
+            if($_SESSION['role'] == "admin"){
+                redirect("Admins/staffaccounts/". $_SESSION['username']);
+            }
+            elseif($_SESSION['role'] == "waiter"){
+                redirect("Waiters/pendingfoodorders/". $_SESSION['username']);
+            }
+            elseif($_SESSION['role'] == "receptionist/". $_SESSION['username']){
+                redirect("Receptionists/reservation");
+            }
+            elseif($_SESSION['role'] == "supervisor/". $_SESSION['username']){
+                redirect("Supervisors/servicerequest");
+            }
+            elseif($_SESSION['role'] == "kitchen"){
+                redirect("Kitchen/foodmenu/". $_SESSION['username']);
+            }
+            elseif($_SESSION['role'] == "customer"){
+                redirect("Customers/reservation/". $_SESSION['username']);
+            }
+            elseif($_SESSION['role'] == "manager"){
+                redirect("Managers/roomdetails/". $_SESSION['username']);
+            }
+
+        }
+
+        public function logout(){
+            unset($_SESSION['user_id']);
+            unset($_SESSION['username']);
+            unset($_SESSION['email']);
+            unset($_SESSION['role']);
+
+            session_destroy();
+            redirect('Users/login');
+
         }
 
 
