8cfbefb83d79e348e7df9a5e3e349283792940d1
visal <visaluni2@gmail.com>
2023-12-15 23:20:34 +0530

    search results shuffle function

--- public/js/customers/reservation.js
+++ public/js/customers/reservation.js
@@ -6,9 +6,28 @@ function togglePopup(id){
     }
 
 function closePopup(id) {
-        document.getElementById(id).classList.remove("active");
-      }
+  document.getElementById(id).classList.remove("active");
+}
 
+
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
 
     /* '''''''''''' slideshow container'''''''''''''''''' */
