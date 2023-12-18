a71d2229feb2bc4dda3a912b86e521c68d1d2633
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2023-12-18 23:35:40 +0530

    add user profile

--- app/views/admins/v_dashboard.php
+++ app/views/admins/v_dashboard.php
@@ -6,4 +6,13 @@
 
 <body>
         <?php require APPROOT. "/views/includes/components/sidenavbar_admin.php" ?>
+
+        <div class="dashboard">
+        <div class="user-profile">
+            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="User Profile Picture">
+            <div class="user-profile-info">
+                <p>John Doe</p>
+                <p>User</p>
+            </div>
+        </div>
 </body>
