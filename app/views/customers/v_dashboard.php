5e3001806d5aa1149d404710b203369be4946a59
visal <visaluni2@gmail.com>
2024-01-05 09:30:48 +0530

    update foodorder coplete

--- app/views/customers/v_dashboard.php
+++ app/views/customers/v_dashboard.php
@@ -29,61 +29,40 @@
 			</div>
 		</div>
 
-		<div class="col-div-3">
-			<div class="box">
-				<p>78<br/><span>Tasks</span></p>
-				<i class="fa fa-tasks box-icon"></i>
-			</div>
-		</div>
 
 		<div class="clearfix"></div>
 		<br/><br/>
 		<div class="col-div-8">
 			<div class="box-8">
 			<div class="content-box">
-				<p>Your Orders <span>Sell All</span></p>
+				<form action="<?php echo URLROOT;?>/Customers/updateOrder" method="POST" >
+				<p>Last Order <span><button>Update</button></span></p></form>
 				<br/>
 				<table>
+					
+							
 					<tr>
 						<th>Item Name</th>
 						<th>Cost</th>
-						<th>Date</th>
+						<th>Quantity</th>
 					</tr>
-					<tr>
-						<td>Alfreds Futterkiste</td>
-						<td>Maria Anders</td>
-						<td>Germany</td>
-					</tr>
-					<tr>
-						<td>Centro comercial Moctezuma</td>
-						<td>Francisco Chang</td>
-						<td>Mexico</td>
-					</tr>
-					<tr>
-						<td>Ernst Handel</td>
-						<td>Roland Mendel</td>
-						<td>Austria</td>
-					</tr>
-					<tr>
-						<td>Island Trading</td>
-						<td>Helen Bennett</td>
-						<td>UK</td>
-					</tr>
-					<tr>
-						<td>Island Trading</td>
-						<td>Helen Bennett</td>
-						<td>UK</td>
-					</tr>
-					<tr>
-						<td>Island Trading</td>
-						<td>Helen Bennett</td>
-						<td>UK</td>
-					</tr>
-					<tr>
-						<td>Island Trading</td>
-						<td>Helen Bennett</td>
-						<td>UK</td>
-					</tr>
+					<?php if(!empty($data)){?>
+						<?php $item=$data;
+							$itemNames = explode(",",$item ->item_name);
+							$itemCosts = explode(",", $item->cost);
+							$quantities = explode(",", $item->quantity);
+							$itemData = array_combine($itemNames, $quantities);
+							for ($i = 0; $i < count($itemNames); $i++){?>
+						<tr>
+							<td><?php echo $itemNames[$i]; ?></td>
+							<td><?php echo $quantities[$i] ; ?></td>
+							<td><?php echo $itemCosts[$i];?>	</td>
+						</tr>
+						<?php } ?>	
+					<?php } 
+						?>		  
+					
+					
 			
 			
 				</table>
