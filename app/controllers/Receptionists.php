a790f44c5c2f43c1e577598d949592af422b1c64
visal <visaluni2@gmail.com>
2024-01-25 00:15:26 +0530

    receptionist room availibility checking complete

--- app/controllers/Receptionists.php
+++ app/controllers/Receptionists.php
@@ -1,8 +1,12 @@
 <?php
 
     class Receptionists extends Controller{
+        protected $middleware;
 
+        protected $receptionistModel;
         public function __construct(){
+
+            $this->receptionistModel = $this->model('M_Receptionist');
             // Load middleware
             $this->middleware = new AuthMiddleware();
             // Check if user is logged in
@@ -10,24 +14,83 @@
             
         }
 
-        public function payment(){
-            $data =[ 
-            ];
-             $this->view('receptionists/v_payments', $data);
-            
-        }
+       // Receptionist Reservation Part''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
         public function reservation(){
-            $data =[ 
-            ];
-             $this->view('receptionists/v_reservation', $data);
             
-        }
-        public function availability(){
-            $data =[ 
-            ];
-             $this->view('receptionists/v_avaiability', $data);
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+                $data=[
+                    'check_in' => trim($_POST['check_in']),
+                    'check_out' => trim($_POST['check_out']),
+                    'room_count' => trim($_POST['room_count']),
+               
+                    'check_in_err' => '',
+                    'check_out_err' => '',
+                    'room_count_err' => '',
+                    'room_type_err' => ''
+                ];
+
+                if(empty($data['check_in'])){
+                    $data['check_in_err'] = 'Please enter check in date';
+                    $_SESSION['toast_type']='info';
+                    $_SESSION['toast_msg']=$data['check_in_err'];
+                    redirect('receptionists/reservation');
+                }
+
+                elseif(empty($data['check_out'])){
+                    $data['check_out_err'] = 'Please enter check out date';
+                    $_SESSION['toast_type']='info';
+                    $_SESSION['toast_msg']=$data['check_out_err'];
+                    redirect('receptionists/reservation');
+                }
+
+                elseif(empty($data['room_count'])){
+                    $data['room_count_err'] = 'Please enter room count';
+                    $_SESSION['toast_type']='info';
+                    $_SESSION['toast_msg']=$data['room_count_err'];
+                    redirect('receptionists/reservation');
+                }
+
+                elseif($data['check_in'] > $data['check_out']){
+                    $data['check_in_err'] = 'Check in date should be less than Check out date';
+                    $_SESSION['toast_type']='info';
+                    $_SESSION['toast_msg']=$data['check_in_err'];
+                    redirect('receptionists/reservation');
+                }
+
+                else{
+
+                    if($output=$this->receptionistModel->checkAvailability($data)){
+
+                        $this->view('receptionists/v_reservation',$output);
+
+                    }
+
+                    else{
+                        $_SESSION['toast_type']='error';
+                        $_SESSION['toast_msg']='Something went wrong';
+                        redirect('receptionists/reservation');
+                    }
+
+
+                }
             
-        }
+            }
+
+            else{
+                    
+                    $this->view('receptionists/v_reservation');
+
+                    if(!empty($_SESSION['toast_type']) && !empty($_SESSION['toast_msg'])){
+                        toastFlashMsg();
+                    }
+            }
+
+        }   
+
+
+        
 
         
     }
