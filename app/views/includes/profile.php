dd42d610eeec6eac77f0538dc8941cd33e72ff7b
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-04-15 10:58:49 +0530

    users profile UI

---
+++ app/views/includes/profile.php
@@ -0,0 +1,33 @@
+<?php   require APPROOT. "/views/includes/components/sidenavbar.php" ?>
+
+
+<form action="">
+    <div class="profile-container">
+        <div class="profile-head">
+            <p>Profile</p>
+            <div class="profile-img">
+                <img src="<?php echo URLROOT; ?>/img/users/profile.jpg" alt="profile"> <br>
+
+                <input type="file" name="propic" id="propic"  >
+            </div>
+        </div>
+
+        <div class="profile-content">
+            <div class="profile-details">
+                <p>Full Name</p>
+                <input type="text" name="name" value="<?php echo $data->name; ?>">
+                <p>Email</p>
+                <input type="text" name="email" value="<?php echo $data->email; ?>">
+                <p>Phone Number</p>
+                <input type="text" name="phone" value="<?php echo $data->phone; ?>">
+                <p>Address</p>
+                <input type="text" name="address" value="<?php echo $data->address; ?>">
+                
+                    
+                <p><button type="submit" >Update</button></p>
+            </div>
+        </div>
+    </form>
+
+    
+    </div>
