02254fc17cc57f0c61a23e1ab1622304fa22a64e
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2024-04-30 05:28:01 +0530

    logo changed

--- app/views/admins/v_generatereports.php
+++ app/views/admins/v_generatereports.php
@@ -9,7 +9,7 @@ require APPROOT . "/views/includes/components/sidenavbar_" . $userRole . ".php";
         <h1>Generate Reports</h1>
     </div>
     <div class="generate-report">
-        <form action="<?php echo URLROOT; ?>/Reports/generateReports" method="POST" id="reportForm">
+        <form action="<?php echo URLROOT; ?>/Reports/generateReports" method ="POST" id="reportForm">
             <div class="form-group">
                 <label for="report_type">Select Report Type:</label>
                 <select name="report_type" id="report_type">
